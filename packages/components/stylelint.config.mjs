9c8c1898847b5f9a582ccc34b6d92ba72e434442
Steven Waller <jstevenwaller@gmail.com>
2025-10-16 09:55:38 -0400

    add style linting

---
+++ packages/components/stylelint.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'selector-class-pattern': null,
+    'custom-property-pattern': null,
+    'comment-empty-line-before': null,
+  },
+}
